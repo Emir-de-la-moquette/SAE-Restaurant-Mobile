9ab2cbca916a53a82ff7b09c88b3b97f95afad6c
TristanChaloine <tristan.chaloine@gmail.com>
2025-03-25 16:54:30 +0100

    test resol conflit ecran_one

--- lib/UI/ecran_one.dart
+++ lib/UI/ecran_one.dart
@@ -17,25 +17,25 @@ class Ecran1 extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     myTasks = context.watch<TaskViewModel>().liste;
-    return ListView.builder(
+     return ListView.builder(
         itemCount : myTasks.length,
         itemBuilder : (BuildContext context, int index){
-          return Card(
-            color: Colors.white,
-            elevation: 7,
-            margin: const EdgeInsets.all(10),
-            child: ListTile (
-              leading:CircleAvatar(backgroundColor: Colors.lightBlue,child:Text(myTasks[index].id.toString()),),
-              title:Text(myTasks[index].title),
-              subtitle: Text(myTasks[index].tags.join(" ")),
-              trailing: IconButton(
-                icon: const Icon(Icons.edit),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => Detail(task: myTasks[index])));                                                      },
+           return Card(
+              color: Colors.white,
+              elevation: 7,
+              margin: const EdgeInsets.all(10),
+              child: ListTile (
+                  leading:CircleAvatar(backgroundColor: Colors.lightBlue,child:Text(myTasks[index].id.toString()),),
+                  title:Text(myTasks[index].title),
+                  subtitle: Text(myTasks[index].tags.join(" ")),
+                  trailing: IconButton(
+                                  icon: const Icon(Icons.edit),
+                                  onPressed: () {
+                                    Navigator.push(context, MaterialPageRoute(builder: (context) => Detail(task: myTasks[index])));                                                      },
+                  ),
               ),
-            ),
-          );
+           );
         }
-    );
+     );
   }
 }
