1c9e9e3897228f29ca91477ac8f85ffea60eb1ad
TristanChaloine <tristan.chaloine@gmail.com>
2025-03-25 16:36:58 +0100

    tentative correction conflit

--- lib/UI/ecran_one.dart
+++ lib/UI/ecran_one.dart
@@ -4,15 +4,11 @@ import 'package:provider/provider.dart';
 import '../models/task.dart';
 import '../viewmodel/task_view_model.dart';
 import 'detail.dart';
-
 class Ecran1 extends StatelessWidget{
-
   //final List<Task> myTasks = Task.generateTask(5);
   late List<Task> myTasks = Task.generateTask(6);
   String tags='';
-
   Ecran1({super.key});
-
   @override
   Widget build(BuildContext context) {
     myTasks = context.watch<TaskViewModel>().liste;
